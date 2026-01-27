884d3fe975f8bb1f064258db197e79bac587bb55
Saiprakash-git <captain191916181111198@gmail.com>
2026-01-27 22:51:19 +0530

    FrontEnd deploy config

--- TTD_Website-Draft2/Faculty_Subjects/faculty-preferences-mern/client/src/utils/api.js
+++ TTD_Website-Draft2/Faculty_Subjects/faculty-preferences-mern/client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'sonner';
 
 const api = axios.create({
-  baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_BASE || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
